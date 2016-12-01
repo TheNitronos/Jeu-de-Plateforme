750ff83f6ea97a2a13dd239f4082e70704bfde0d
Unknown <samuel.chassot@bluewin.ch>
2016-12-01 17:46:45 +0100

    exit a tester

--- src/platform/game/Exit.java
+++ src/platform/game/Exit.java
@@ -4,7 +4,7 @@
  */
 
 
-package platform.game.level;
+package platform.game;
 
 import platform.game.Block;
 import platform.game.Signal;
@@ -13,9 +13,37 @@ import platform.util.Box;
 import platform.util.Sprite;
 import platform.util.Input;
 import platform.util.Output;
+import platform.game.level.*;
 
 
 
-public class Exit {
-
+public class Exit extends Actor {
+	private Level level;
+	private Signal signal;
+	private Vector position;
+	private final double SIZE = 1.0;
+	
+	public Exit(Vector pos, Signal signal, Level lvl){
+		this.signal = signal;
+		position = pos;
+		level = lvl;
+	}
+	
+	@Override
+	public Box getBox(){
+		return new Box(position, SIZE, SIZE);
+	}
+	
+	@Override
+	public int getPriority(){
+		return 10;
+	}
+	
+	public void interact(Actor other){
+		super.interact(other);
+		if(getBox().isColliding(other.getBox()) && other.getClass().getName() == "platform.game.Player"){
+			getWorld().setNextLevel(level);
+			getWorld().nextLevel();
+		}
+	}
 }
