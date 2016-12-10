2718b79387d61179a0df47addd06f64075faac33
Unknown <thenitronos@gmail.com>
2016-12-10 18:11:59 +0100

    documentation de Door

--- src/platform/game/Door.java
+++ src/platform/game/Door.java
@@ -8,42 +8,44 @@ import platform.util.Input;
 import platform.util.Output;
 
 
+/**
+ * bloc qui peut disparaître associé à une clef
+ * le signal true signifie la disparition du bloc et inversément
+ */
 public class Door extends Block implements Signal {
+	//signal auquel réagit le bloc
 	private final Signal signal;
-
-
+	//état d'activité du bloc : existant ou disparu
 	private boolean active;
 	
-	
-	public Door(Box box, Signal signal, Sprite sprite){
-		super(box, sprite);
-		this.signal = signal;
+	public Door(Box nBox, Signal nSignal, Sprite nSprite){
+		super(nBox, nSprite);
+		signal = nSignal;
 		active = false;
-	}
-	
-	@Override
-	public Box getBox(){
-		return super.getBox();
 	}
 	
 	@Override
 	public boolean isActive(){
 		return active;
 	}
+	
 	@Override
 	public void draw(Input input, Output output){
+		//si le signal n'est pas true, on affiche le bloc
 		if (!signal.isActive()){
-			super.draw(input, output);			
+			super.draw(input, output);	
+			
 		}
 	}
 	
 	@Override
 	public boolean isSolid(){
-		if (signal.isActive()){
+		//si le bloc a disparu, il n'est plus solide sinon il l'est
+		if (signal.isActive()) {
 			return false;
 		}
-		else{
-			return super.isSolid();
+		else {
+			return true;
 		}
 	}
 }
