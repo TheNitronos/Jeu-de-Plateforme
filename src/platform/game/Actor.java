b0a1017e4bc7747a4ba3ae1281584f2a7bab1c29
Unknown <thenitronos@gmail.com>
2016-12-10 11:44:51 +0100

    documentation de Actor

--- src/platform/game/Actor.java
+++ src/platform/game/Actor.java
@@ -7,38 +7,64 @@ import platform.util.Output;
 import platform.util.Sprite;
 
 /**
- * Base class of all simulated actors, attached to a world.
+ * Classe de base de tous les acteurs simulés dans une monde (World)
  */
 public abstract class Actor implements Comparable<Actor> {
 	
+	//monde auquel est rattaché l'acteur
 	private World world;
 
-	//pour evoluer au cours du temps	
+	//faire évoluer l'acteur au cours du temps	
 	public void update(Input input) {}
 	
-	//pour evoluer avant la physique et apres le dessin
+	//faire évoluer l'acteur avant la physique et après le dessin
 	public void preUpdate(Input input) {}
 	
+	//faire évoluer l'acteur après la physique
 	public void postUpdate(Input input) {}
 	
-	//pour etre dessiné	
+	//dessiner l'acteur
 	public void draw(Input input, Output output) {}
 	
+	/**
+	 * @return int, la valeur d'importance de l'acteur décrite par une 'priorité'
+	 */
 	public abstract int getPriority();
 	
-	
+	/**
+	 * Méthode d'interactions entre les acteurs lorsque des 'dégâts' sont infligés
+	 * @param instigator, instigateur des dégâts
+	 * @param type, type de dégâts (air, feu,...)
+	 * @param amount, quantité de dégâts
+	 * @param location, localisation des dégâts
+	 * @return boolean, true si le dégât a eu effet, false sinon
+	 */
 	public boolean hurt(Actor instigator, Damage type, double amount, Vector location){
+		//par defaut, lors d'une interaction, les dégâts n'ont pas d'effets
 		return false;
 	}
 	
+	/**
+	 * Méthode d'interactions entre les acteurs, si l'on peut passer à travers ou pas
+	 * s'il est solide, on ne peut pas passer à travers
+	 * s'il n'est pas solide, on peut passer à travers
+	 * @return boolean, true si l'acteur est solide, false sinon
+	 */
 	public boolean isSolid(){
+		//par défaut, aucun acteur n'est solide
 		return false;
 	}
 	
+	/**
+	 * @return Box, 'boîte' dans laquelle est inscrit l'acteur
+	 */
 	public Box getBox(){
 		return null;
 	}
 	
+	/**
+	 * @return Vector, vecteur de la position du centre de la box dans laquelle est inscrit l'acteur
+	 */
 	public Vector getPosition(){
 		Box box = getBox();
 		
@@ -51,6 +77,14 @@ public abstract class Actor implements Comparable<Actor> {
 	
 	public void interact(Actor other) {}
 	
+	/**
+	 * méthode de comparaison de la priorité des acteurs
+	 * @other Actor, acteur auquel 'this' est comparé
+	 * @return int, résultat de la comparaison des priorités
+	 * 1 : l'acteur en question a une plus petite priorité que celui auquel il est comparé
+	 * -1 : l'acteur en question a une plus grande priorité que celui auquel il est comparé
+	 * 0 : les deux acteurs ont la même priorité
+	 */
 	@Override
 	public int compareTo(Actor other){
 		if(getPriority() < other.getPriority()){
@@ -64,22 +98,40 @@ public abstract class Actor implements Comparable<Actor> {
 		}
 	}
 	
+	/**
+	 * @param nWorld, nouveau monde à associer à l'acteur
+	 */
 	public void register(World nWorld){
 		world = nWorld;
 	}
 	
+	/**
+	 * remet le monde auquel est associé l'acteur à null
+	 */
 	public void unregister(){
 		world = null;
 	}
 	
+	/**
+	 * @return World, monde dans lequel évolue l'acteur
+	 */
 	protected World getWorld(){
 		return world;
 	}
 	
+	/**
+	 * Récupération de la sprite correspondante
+	 * @param name, nom du fichier de la sprite sans extension
+	 * @return Sprite, 'image' chargée
+	 */
 	public Sprite getSprite(String name){
 		return world.getLoader().getSprite(name);
 	}
 	
+	/**
+	 * Vitesse verticale de l'acteur
+	 * @return double, vitesse sur l'axe y
+	 */
 	public double getVelocityY(){
 		return 0.0;
 	}
