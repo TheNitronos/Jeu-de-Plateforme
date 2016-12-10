1ff02784a6b0e658b3d3d8260aac8fa52bc6a155
Unknown <thenitronos@gmail.com>
2016-12-10 18:25:50 +0100

    documentation Exit

--- src/platform/game/Exit.java
+++ src/platform/game/Exit.java
@@ -10,41 +10,64 @@ import platform.game.Signal;
 import platform.game.level.Level;
 import platform.game.Player;
 
-
+/**
+ * Porte de sortie
+ */
 public class Exit extends Actor {
+	//prochain niveau auquel on passe en entrant dans la porte
 	private final Level LEVEL;
+	//signal associé à la porte
 	private final Signal SIGNAL;
 	private final Vector POSITION;
-	private final double SIZE = 1.0;
+	private final double SIZE;
 	
 	public Exit(Vector nPosition, Signal nSignal, Level nLevel) {
 		SIGNAL = nSignal;
 		POSITION = nPosition;
 		LEVEL = nLevel;
+		SIZE = 1.0;
 	}
 	
 	@Override
 	public Box getBox() {
+		/*
+		 * création d'une instance de Box
+		 * avec le centre de la porte et ses
+		 * dimensions
+		 */
 		return new Box(POSITION, SIZE, SIZE);
 	}
 	
 	@Override
 	public int getPriority() {
+		/*
+		 * si le signal est actif, la priorité croît ainsi le joueur
+		 * passe derrière, ce qui donne une impression de passer derrière
+		 * et la porte peut interagir avec le joueur du fait que sa propre priorité
+		 * est plus grande que celle du joueur
+		 */
 		if (SIGNAL.isActive()) {
 			return 60;
 		}
 		else{
 			return -3;
 		}
-		
 	}
 	
 	@Override
 	public void interact(Actor other) {
 		super.interact(other);
 		
+		/*
+		 * si la porte est en contact avec la Box du joueur
+		 * on passe au prochain niveau <LEVEL>
+		 * il n'y a pas besoin de contrôler l'état du signal
+		 * puisque si le signal n'est pas activé et ne donne pas
+		 * la priorité à l'instance d'executer cette méthode,
+		 * celle-ci n'a pas lieu
+		 */
 		if (getBox().isColliding(other.getBox()) 
-				&& (other instanceof Player) && SIGNAL.isActive()) {
+				&& (other instanceof Player)) {
 			
 			getWorld().setNextLevel(LEVEL);
 			getWorld().nextLevel();
@@ -57,6 +80,9 @@ public class Exit extends Actor {
 		
 		String name = "door.";
 		
+		/*
+		 * selon l'état de la porte, on affiche l'une ou l'autre sprite
+		 */
 		if (SIGNAL.isActive()) {
 			name += "open";
 		} else {
