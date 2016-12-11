32a65f4e005df98e84dc7234e06391b7d3acd2e8
Unknown <thenitronos@gmail.com>
2016-12-11 14:53:27 +0100

    documentation de or

--- src/platform/game/Or.java
+++ src/platform/game/Or.java
@@ -2,20 +2,26 @@ package platform.game;
 
 import platform.game.Signal;
 
+/**
+ * classe qui représente le "or" de 2 signaux
+ * dès qu'un signal est true, le signal des 2
+ * devient true
+ */
 public class Or implements Signal{
-	private final Signal signal1;
-	private final Signal signal2;
+	private final Signal SIGNAL1;
+	private final Signal SIGNAL2;
 	
-	public Or(Signal signal1, Signal signal2){
-		if (signal1==null || signal2 == null){
+	public Or(Signal nSignal1, Signal nSignal2){
+		if (nSignal1 == null || nSignal2 == null){
 			throw new NullPointerException();
 		}
-		this.signal1 = signal1;
-		this.signal2 = signal2;
+		
+		SIGNAL1 = nSignal1;
+		SIGNAL2 = nSignal2;
 	}
 	
 	@Override
-	public boolean isActive(){
-		return signal1.isActive() || signal2.isActive();
+	public boolean isActive() {
+		return SIGNAL1.isActive() || SIGNAL2.isActive();
 	}
 }
