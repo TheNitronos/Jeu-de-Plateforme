f4e83461b7c9c2ffddcf540ce133233b03ba0060
Unknown <thenitronos@gmail.com>
2016-12-11 15:34:29 +0100

    documentation spike

--- src/platform/game/Spike.java
+++ src/platform/game/Spike.java
@@ -6,27 +6,34 @@ import platform.util.Sprite;
 import platform.util.Output;
 import platform.util.Vector;
 
+/**
+ * piques qui infligent de la perte de vie et qui
+ * font sauter le player de douleur
+ */
 public class Spike extends Actor {
 	private final double SIZE = 1.0;
-	private Vector position;
+	private final Vector POSITION;
 	
-	public Spike(Vector pos) {
-		position = pos;
+	public Spike(Vector nPosition) {
+		POSITION = nPosition;
 	}
 	
 	@Override
 	public int getPriority() {
+		//haute priorité pour interagir avec le joueur
 		return 333;
 	}
 	
 	@Override
 	public Box getBox() {
-		return new Box(position, SIZE, SIZE/2);
+		//la hauteur est deux foix plus petite que la largeur
+		return new Box(POSITION, SIZE, SIZE/2);
 	}
 	
 	@Override
 	public void draw(Input input, Output output) {
 		Sprite sprite = this.getSprite("spikes");
+		
 		if (sprite != null) {
 			output.drawSprite(sprite, getBox());
 		}
@@ -34,8 +41,9 @@ public class Spike extends Actor {
 	
 	@Override
 	public void interact(Actor other) {
+		//si un acteur tombe verticalement sur les piques, il subit des dégâts physiques
 		if(other.getVelocityY() < -1 && getBox().isColliding(other.getBox())) {
-			other.hurt(this, Damage.PHYSICAL, 2.0 , position);
+			other.hurt(this, Damage.PHYSICAL, 2.0 , POSITION);
 		}	
 	}
 }
