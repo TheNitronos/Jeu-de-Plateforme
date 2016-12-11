787854bf50abdadb31b80243dc09e10b599fd39f
Unknown <thenitronos@gmail.com>
2016-12-11 14:47:32 +0100

    documentation mover

--- src/platform/game/Mover.java
+++ src/platform/game/Mover.java
@@ -8,34 +8,45 @@ import platform.util.Sprite;
 import platform.util.Input;
 import platform.util.Output;
 
-public class Mover extends Block{
-	private Vector on;
-	private Vector off;
-	private Signal signal;
+/**
+ * Bloc qui peuvent bouger sous l'interaction d'autres signaux
+ */
+public class Mover extends Block {
+	//position du bloc au signal actif
+	private final Vector ON;
+	//position du bloc au signal inactif
+	private final Vector OFF;
+	//signal correspondant
+	private final Signal SIGNAL;
+	private final Sprite SPRITE;
+	//rapport de déplacement intermédiaire
 	private double current;
-	private Sprite sprite;
 
 	
-	public Mover(Box box, Sprite sprite, Vector on, Signal signal){
-		super(box, sprite);
-		this.off = box.getCenter();
-		this.on = on;
-		this.signal = signal;
-		this.sprite = sprite;
+	public Mover(Box nBox, Sprite nSprite, Vector nOn, Signal nSignal){
+		super(nBox, nSprite);
+		OFF = nBox.getCenter();
+		ON = nOn;
+		SIGNAL = nSignal;
+		SPRITE = nSprite;
 	}
 	
 	@Override
-	public void update(Input input){
+	public void update(Input input) {
 		super.update(input);
-		if (signal.isActive()){
+		
+		if (SIGNAL.isActive()) {
+			//si le signal est actif on augmente le rapport de deplacement une fois à 100% on le laisse à 1
 			current += input.getDeltaTime();
+			
 			if (current > 1.0){
 				current = 1.0;
 			}
-		}
-		else{
+		} else {
+			//inverse du cas où le signal est actif
 			current -= input.getDeltaTime();
-			if (current < 0.0){
+			
+			if (current < 0.0) {
 				current = 0.0;
 			}
 		}
@@ -43,17 +54,20 @@ public class Mover extends Block{
 	
 	@Override
 	public Box getBox(){
-		if (signal.isActive()){
-			return super.getBox().add(on.sub(off).mul(current));
-		}
-		else {
-			return super.getBox().add(off.sub(on).mul(current).mul(-1.0));
+		/*
+		 * la Box correspondante est la boîte initiale que l'on déplace en fonction du rapport
+		 * de déplacement et des deux extrêmes de position
+		 */
+		if (SIGNAL.isActive()){
+			return super.getBox().add(ON.sub(OFF).mul(current));
+		} else {
+			return super.getBox().add(OFF.sub(ON).mul(current).mul(-1.0));
 		}
 	}
 	
 	@Override
 	public void draw(Input input, Output output){
-		output.drawSprite(sprite, getBox());
-		
+		//mise à jour de la méthode draw pour qu'elle redessine en fontion de la nouvelle boîte 
+		output.drawSprite(SPRITE, getBox());
 	}	
 }
